c5bd1bc7353eaf24733db60530afddfd990982c0
Lucas-Cassan <lucascassan@outlook.com>
2022-04-25 15:42:09 +0200

    First commit

---
+++ App.tsx
@@ -0,0 +1,21 @@
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+import useCachedResources from "./hooks/useCachedResources";
+import useColorScheme from "./hooks/useColorScheme";
+import TabOneScreen from "./screens/TabOneScreen";
+
+export default function App() {
+  const isLoadingComplete = useCachedResources();
+  const colorScheme = useColorScheme();
+
+  if (!isLoadingComplete) {
+    return null;
+  } else {
+    return (
+      <SafeAreaProvider>
+        <TabOneScreen />
+      </SafeAreaProvider>
+    );
+  }
+}
